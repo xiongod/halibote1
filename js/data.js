500b650538ef441e06cb7b16a469586bdaa3dc16
张一雄 <erhu02@sina.com>
2024-05-21 09:53:29 +0800

    zyx

---
+++ js/data.js
@@ -0,0 +1,386 @@
+[
+    {
+        "title": "1.【预告】《哈利·波特与魔法石》有声预告片.m4a",
+        "url": "https://xiongod.github.io/halibote1/01.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 1
+    },
+    {
+        "title": "2.【试听】《哈利·波特》第一部 第1集 大难不死的男孩1.m4a",
+        "url": "https://xiongod.github.io/halibote1/02.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 2
+    },
+    {
+        "title": "3.【试听】《哈利·波特》第一部 第2集 大难不死的男孩2.m4a",
+        "url": "https://xiongod.github.io/halibote1/03.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 3
+    },
+    {
+        "title": "4.【试听】《哈利·波特》第一部 第3集 悄悄消失的玻璃1.m4a",
+        "url": "https://xiongod.github.io/halibote1/04.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 4
+    },
+    {
+        "title": "5.【试听】《哈利·波特》第一部 第4集 悄悄消失的玻璃2.m4a",
+        "url": "https://xiongod.github.io/halibote1/05.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 5
+    },
+    {
+        "title": "6.【试听】《哈利·波特》第一部 第5集 猫头鹰传书1.m4a",
+        "url": "https://xiongod.github.io/halibote1/06.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 6
+    },
+    {
+        "title": "7.【试听】《哈利·波特》第一部 第6集 猫头鹰传书2.m4a",
+        "url": "https://xiongod.github.io/halibote1/07.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 7
+    },
+    {
+        "title": "8.【试听】《哈利·波特》第一部 第7集 钥匙保管员1.m4a",
+        "url": "https://xiongod.github.io/halibote1/08.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 8
+    },
+    {
+        "title": "9.【试听】《哈利·波特》第一部 第8集 钥匙保管员2.m4a",
+        "url": "https://xiongod.github.io/halibote1/09.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 9
+    },
+    {
+        "title": "10.【试听】《哈利·波特》第一部 第9集 对角巷1.m4a",
+        "url": "https://xiongod.github.io/halibote1/10.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 10
+    },
+    {
+        "title": "11.《哈利·波特》第一部 第10集 对角巷2.m4a",
+        "url": "https://xiongod.github.io/halibote1/11.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 11
+    },
+    {
+        "title": "12.《哈利·波特》第一部 第11集 对角巷3.m4a",
+        "url": "https://xiongod.github.io/halibote1/12.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 12
+    },
+    {
+        "title": "13.《哈利·波特》第一部 第12集 对角巷4.m4a",
+        "url": "https://xiongod.github.io/halibote1/13.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 13
+    },
+    {
+        "title": "14.【预告】魔法铁三角.m4a",
+        "url": "https://xiongod.github.io/halibote1/14.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 14
+    },
+    {
+        "title": "15.《哈利·波特》第一部 第13集 从9¾站台开始的旅程1.m4a",
+        "url": "https://xiongod.github.io/halibote1/15.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 15
+    },
+    {
+        "title": "16.《哈利·波特》第一部 第14集 从9¾站台开始的旅程2.m4a",
+        "url": "https://xiongod.github.io/halibote1/16.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 16
+    },
+    {
+        "title": "17.《哈利·波特》第一部 第15集 从9¾站台开始的旅程3.m4a",
+        "url": "https://xiongod.github.io/halibote1/17.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 17
+    },
+    {
+        "title": "18.《哈利·波特》第一部 第16集 从9¾站台开始的旅程4.m4a",
+        "url": "https://xiongod.github.io/halibote1/18.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 18
+    },
+    {
+        "title": "19.【预告】霍格沃茨师生群像.m4a",
+        "url": "https://xiongod.github.io/halibote1/19.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 19
+    },
+    {
+        "title": "20.《哈利·波特》第一部 第17集 分院帽1.m4a",
+        "url": "https://xiongod.github.io/halibote1/20.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 20
+    },
+    {
+        "title": "21.《哈利·波特》第一部 第18集 分院帽2.m4a",
+        "url": "https://xiongod.github.io/halibote1/21.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 21
+    },
+    {
+        "title": "22.《哈利·波特》第一部 第19集 魔药课老师1.m4a",
+        "url": "https://xiongod.github.io/halibote1/22.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 22
+    },
+    {
+        "title": "23.《哈利·波特》第一部 第20集 魔药课老师2.m4a",
+        "url": "https://xiongod.github.io/halibote1/23.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 23
+    },
+    {
+        "title": "24.《哈利·波特》第一部 第21集 午夜决斗1.m4a",
+        "url": "https://xiongod.github.io/halibote1/24.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 24
+    },
+    {
+        "title": "25.《哈利·波特》第一部 第22集 午夜决斗2.m4a",
+        "url": "https://xiongod.github.io/halibote1/25.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 25
+    },
+    {
+        "title": "26.《哈利·波特》第一部 第23集 午夜决斗3.m4a",
+        "url": "https://xiongod.github.io/halibote1/26.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 26
+    },
+    {
+        "title": "27.《哈利·波特》第一部 第24集 万圣节前夕1.m4a",
+        "url": "https://xiongod.github.io/halibote1/27.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 27
+    },
+    {
+        "title": "28.《哈利·波特》第一部 第25集 万圣节前夕2.m4a",
+        "url": "https://xiongod.github.io/halibote1/28.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 28
+    },
+    {
+        "title": "29.【预告】魁地奇比赛.m4a",
+        "url": "https://xiongod.github.io/halibote1/29.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 29
+    },
+    {
+        "title": "30.《哈利·波特》第一部 第26集 魁地奇比赛1.m4a",
+        "url": "https://xiongod.github.io/halibote1/30.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 30
+    },
+    {
+        "title": "31.《哈利·波特》第一部 第27集 魁地奇比赛2.m4a",
+        "url": "https://xiongod.github.io/halibote1/31.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 31
+    },
+    {
+        "title": "32.《哈利·波特》第一部 第28集 厄里斯魔镜1.m4a",
+        "url": "https://xiongod.github.io/halibote1/32.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 32
+    },
+    {
+        "title": "33.《哈利·波特》第一部 第29集 厄里斯魔镜2.m4a",
+        "url": "https://xiongod.github.io/halibote1/33.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 33
+    },
+    {
+        "title": "34.《哈利·波特》第一部 第30集 厄里斯魔镜3.m4a",
+        "url": "https://xiongod.github.io/halibote1/34.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 34
+    },
+    {
+        "title": "35.《哈利·波特》第一部 第31集 尼可·勒梅1.m4a",
+        "url": "https://xiongod.github.io/halibote1/35.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 35
+    },
+    {
+        "title": "36.《哈利·波特》第一部 第32集 尼可·勒梅2.m4a",
+        "url": "https://xiongod.github.io/halibote1/36.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 36
+    },
+    {
+        "title": "37.《哈利·波特》第一部 第33集 挪威脊背龙——诺伯1.m4a",
+        "url": "https://xiongod.github.io/halibote1/37.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 37
+    },
+    {
+        "title": "38.《哈利·波特》第一部 第34集 挪威脊背龙——诺伯2.m4a",
+        "url": "https://xiongod.github.io/halibote1/38.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 38
+    },
+    {
+        "title": "39.《哈利·波特》第一部 第35集 禁林1.m4a",
+        "url": "https://xiongod.github.io/halibote1/39.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 39
+    },
+    {
+        "title": "40.《哈利·波特》第一部 第36集 禁林2.m4a",
+        "url": "https://xiongod.github.io/halibote1/40.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 40
+    },
+    {
+        "title": "41.《哈利·波特》第一部 第37集 禁林3.m4a",
+        "url": "https://xiongod.github.io/halibote1/41.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 41
+    },
+    {
+        "title": "42.《哈利·波特》第一部 第38集 穿越活板门1.m4a",
+        "url": "https://xiongod.github.io/halibote1/42.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 42
+    },
+    {
+        "title": "43.《哈利·波特》第一部 第39集 穿越活板门2.m4a",
+        "url": "https://xiongod.github.io/halibote1/43.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 43
+    },
+    {
+        "title": "44.《哈利·波特》第一部 第40集 穿越活板门3.m4a",
+        "url": "https://xiongod.github.io/halibote1/44.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 44
+    },
+    {
+        "title": "45.《哈利·波特》第一部 第41集 穿越活板门4.m4a",
+        "url": "https://xiongod.github.io/halibote1/45.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 45
+    },
+    {
+        "title": "46.《哈利·波特》第一部 第42集 双面人1.m4a",
+        "url": "https://xiongod.github.io/halibote1/46.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 46
+    },
+    {
+        "title": "47.《哈利·波特》第一部 第43集 双面人2.m4a",
+        "url": "https://xiongod.github.io/halibote1/47.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 47
+    },
+    {
+        "title": "48.《哈利·波特》第一部 第44集 双面人3.m4a",
+        "url": "https://xiongod.github.io/halibote1/48.m4a",
+        "duration": "",
+        "published": "2024-05-21",
+        "createTime": "",
+        "chapter": 48
+    }
+]
